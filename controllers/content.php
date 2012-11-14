82526b5a5166ed7f4dd65bd83fcc6cd5e0731378
adias <adias@sowebsites.com.br>
2012-11-14 16:04:15 -0200

    Update at 14/11

--- controllers/content.php
+++ controllers/content.php
@@ -1,46 +1,28 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 class content extends Admin_Controller {
-
 	//--------------------------------------------------------------------
-
-
 	public function __construct()
 	{
 		parent::__construct();
-
 		$this->auth->restrict('Simplenews.Content.View');
-		$this->lang->load('simplenews');
-		
+		$this->lang->load('simplenews');				
+		$this->load->helper('form');
 		$this->load->model('news_model', null, true);
-		$this->load->model('category_model', null, true);
-		
+		$this->load->model('category_model', null, true);		
 		Template::set_block('sub_nav', 'content/_sub_nav');
 	}
-
 	//--------------------------------------------------------------------
-
-
-
-	/*
-		Method: index()
-
+	/*	Method: index()
 		Displays a list of form data.
 	*/
 	public function index()
 	{
-
 		Template::set('toolbar_title', 'Manage simplenews');
 		Template::render();
 	}
-
 	//--------------------------------------------------------------------
-
-
-
-	/*
-		Method: create()
-
+	/*  Method: create()
 		Creates a simplenews object.
 	*/
 	public function create()
@@ -52,8 +34,39 @@ class content extends Admin_Controller {
 		Template::set('toolbar_title', lang('simplenews_create') . ' simplenews');
 		Template::render();
 	}
-
 	//--------------------------------------------------------------------
+	
+	
+	public function editnews() {
+		
+//		$this->auth->restrict('simplenews.Content.Edit');
+
+		$id = $this->uri->segment(5);
+		if (empty($id)){
+			Template::set_message(lang('simplenews_invalid_id'), 'error');
+			redirect(SITE_AREA .'/content/simplenews/');
+		}
+		if ($this->input->post('submit')) {
+			if ($this->save_item('update', $id)) {
+				// Log the activity
+				$this->activity_model->log_activity($this->current_user->id, lang('simplenews_act_edit_record').': ' . $id . ' : ' . $this->input->ip_address(), 'simplenews');
+				Template::set_message(lang('simplenews_edit_success'), 'success');
+			} else {
+				Template::set_message(lang('simplenews_edit_failure') . $this->fields_model->error, 'error');
+			}
+		}
+		
+		$category = $this->category_model->find_all();
+		Template::set('categories', $category);		
+		
+		$editnewsdata = $this->news_model->find($id);
+		Template::set('news', $editnewsdata);
+		
+		Template::set('toolbar_title', lang('simplenews_edit') . ' Itens');
+		Template::render();	
+				
+	}
+
 
 
 
@@ -80,40 +93,7 @@ class content extends Admin_Controller {
 	}	
 	//--------------------------------------------------------------------
 	
-	public function editnews()
-	{
-//		$this->auth->restrict('simplenews.Content.Edit');
-		$id = $this->uri->segment(5);
-		if (empty($id))
-		{
-			Template::set_message(lang('simplenews_invalid_id'), 'error');
-			redirect(SITE_AREA .'/content/simplenews/');
-		}
-
-		if ($this->input->post('submit'))
-		{
-			if ($this->save_item('update', $id))
-			{
-				// Log the activity
-				$this->activity_model->log_activity($this->current_user->id, lang('simplenews_act_edit_record').': ' . $id . ' : ' . $this->input->ip_address(), 'simplenews');
-
-				Template::set_message(lang('simplenews_edit_success'), 'success');
-			}
-			else
-			{
-				Template::set_message(lang('simplenews_edit_failure') . $this->fields_model->error, 'error');
-			}
-		}
-		
-		Template::set('editnews', $this->news_model->find($id));
-		
-		$category = $this->category_model->find_all();
-		Template::set('getcategory', $category);
-		
-		Template::set('toolbar_title', lang('simplenews_edit') . ' Itens');
-		Template::render();
-	}
-
+	
 
 
 	/*
--- views/content/editnews.php
+++ views/content/editnews.php
@@ -7,43 +7,54 @@
 <?php endif; ?>
 
 <?php // Change the css classes to suit your needs
-	if( isset($editnews) ) { $editnews = (array)$editnews; }
-	$id = isset($result['id']) ? $result['id'] : '';
+	if( isset($news) ) { $news = (array)$news; }
+	$id = isset($news['id']) ? $news['id'] : '';
+?>
+
+<?php // Change the css classes to suit your needs
+	if( isset($categories) ) { $categories = (array)$categories; }
+	$id = isset($categories['id']) ? $categories['id'] : '';
 ?>
 
 <div class="admin-box">
+
     <h3><?php echo lang('edit')?></h3>
     
 	<?php echo form_open($this->uri->uri_string(), 'class="form-horizontal"'); ?>
     <fieldset>
-
-        <div class="form-actions">
-           
-			<?php echo form_label('category_id', 'category_id'); ?>
-			<select name="category_id">
-                <option value="1">First</option>
-                <option value="0">Second</option>
-			</select>
+    <div class="form-actions">  
+    
+     <!-- BOF CATEGORIES -->
+    <select name="category_id">    
+	<?php foreach ($categories as $category) : ?>
+        <option value="<?php echo $category->id ?>" <?php echo set_select('category_id', $category->id ) ?>>
+            <?php echo $category->category_name ?>
+        </option>
+    <?php endforeach; ?>
+    </select>
+    <!-- EOF CATEGORIES -->
+   
+	<?php echo form_label('id', 'id'); ?>            
+    <input type="text" name="title" id="" value="<?php echo set_value('id', isset($news['id']) ? $news['id'] : ''); ?>" />
             
-			<?php echo form_label('id', 'id'); ?>
-            <input type="text" name="title" id="" 
-            value="<?php echo set_value('id', isset($editnews['id']) ? $editnews['id'] : ''); ?>" />
+    <?php echo form_label('category_id', 'category_id'); ?>
+    <input type="text" name="title" id="" 
+	value="<?php echo set_value('category_id', isset($news['category_id']) ? $news['category_id'] : ''); ?>" />
             
-            <?php echo form_label('title', 'title'); ?>
-            <input type="text" name="title" id="" 
-            value="<?php echo set_value('title', isset($editnews['title']) ? $editnews['title'] : ''); ?>" />
-
-            <?php echo form_label('status', 'status'); ?>
-			<select name="item_featured">
-                <option value="1">yes</option>
-                <option value="0">no</option>
-			</select>
+    <?php echo form_label('title', 'title'); ?>
+    <input type="text" name="title" id="" 
+    value="<?php echo set_value('title', isset($news['title']) ? $news['title'] : ''); ?>" />
             
-            <br/><br/>            
-            <input type="submit" name="submit" class="btn btn-primary" value="Save" />
-				<?php echo lang('simplenews_or'); ?> <?php echo anchor(SITE_AREA .'/content/simplenews/', lang('simplenews_cancel'), 'class="btn btn-warning"'); ?>
-            
-        </div>
+	<?php echo form_label('status', 'status'); ?>
+    <input type="text" name="title" id="" 
+    value="<?php echo set_value('status', isset($news['status']) ? $news['status'] : ''); ?>" />
+	
+	<?php // echo form_label('status', 'status'); ?>
+    <br/><br/>            
+    <input type="submit" name="submit" class="btn btn-primary" value="Save" />
+	<?php echo lang('simplenews_or'); ?> <?php echo anchor(SITE_AREA .'/content/simplenews/', lang('simplenews_cancel'), 'class="btn btn-warning"'); ?>
+    
+    </div>
     </fieldset>
     <?php echo form_close(); ?>
 </div>
